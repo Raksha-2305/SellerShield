44f49bbe43768586c02f0a306479f8d14567c771
Raksha-2305 <cssraksha9035@gmail.com>
2026-09-17 22:36:50 +0530

    Create FlowpayBackendApplicationTests.java

---
+++ src/test/java/com/flowpay/flowpaybackend/FlowpayBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flowpay.flowpaybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowpayBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
