fe174af2c698e6ad53512c30d4c8c2ef25becc21
Raksha-2305 <cssraksha9035@gmail.com>
2026-09-17 22:29:03 +0530

    Create FlowpayBackendApplication.java

---
+++ src/main/java/com/flowpay/flowpaybackend/FlowpayBackendApplication.java
@@ -0,0 +1,13 @@
+package com.flowpay.flowpaybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowpayBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowpayBackendApplication.class, args);
+	}
+
+}
